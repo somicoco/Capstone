2dc58c75854b093a2ad8909782758d23ff2bd775
sindasom <zaqaziq@naver.com>
2024-05-29 00:07:16 +0900

    05-29(test)

---
+++ lib/test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextScreen extends StatefulWidget {
+  const TextScreen({super.key});
+
+  @override
+  State<TextScreen> createState() => _TextScreenState();
+}
+
+class _TextScreenState extends State<TextScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
