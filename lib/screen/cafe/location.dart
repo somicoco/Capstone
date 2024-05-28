0d52ebab48c569e6751ae45985f5e9bae8717147
sindasom <zaqaziq@naver.com>
2024-05-28 13:43:38 +0900

    dasom 0528

---
+++ lib/screen/cafe/location.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter_naver_map/flutter_naver_map.dart';
+//
+// class LocationClass extends NLatLng {
+//   const LocationClass({super.key});
+//
+//   @override
+//   State<LocationClass> createState() => _LocationClassState();
+// }
+//
+// class _LocationClassState extends State<LocationClass> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
